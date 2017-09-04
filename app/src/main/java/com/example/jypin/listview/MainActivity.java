d17a1f5d54c46fe2849291853247bfcc0ed7d164
jypin <apple80177@gmail.com>
2017-09-04 16:05:52 +0800

    add textfilterenabled & selector

--- app/src/main/java/com/example/jypin/listview/MainActivity.java
+++ app/src/main/java/com/example/jypin/listview/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView txtResult;
     private ListView lstPrefer;
-    String [] Balls=new String [] {"籃球", "足球", "棒球", "其他"};
+    String [] Balls=new String [] {"basketball", "soccer", "baseball", "籃球", "足球", "棒球", "其他"};
 
 
     @Override
@@ -22,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
         txtResult=(TextView)findViewById(R.id.txtResult);
         lstPrefer=(ListView)findViewById(R.id.lstPrefer);
+
+        lstPrefer.setTextFilterEnabled(true);//啟用按鍵過濾功能
+        lstPrefer.setSelector(R.drawable.green);//改變選取選項的背景色為圖片
 
         ArrayAdapter<String> adapterBalls=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Balls);
 
